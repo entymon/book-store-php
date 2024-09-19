8968d3153fdebe55914978125e47e65d3e065800
entymon <pawel.olejniczak.i@gmail.com>
2024-09-19 13:08:58 +0200

    add simple CRUD with simple validation

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -2,11 +2,13 @@
 
 namespace App\Controller;
 
+use App\Exception\ValidationException;
 use App\Entity\Book;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class BookController extends AbstractController
@@ -30,25 +32,40 @@ class BookController extends AbstractController
     #[Route('/books', name: 'book_create', methods:['post'] )]
     public function create(EntityManagerInterface $entityManager, Request $request): JsonResponse
     {
-        $parameters = json_decode($request->getContent(), true);
-        // $bookValidator = $this->get('app.validation_iban');
-        // var_dump($parameters); die;
+        try {
+            $parameters = json_decode($request->getContent(), true);
+            $this->simpleDataValdiation($parameters);
 
-        $book = new Book();
-        $book->setTitle($parameters['title']);
-        $book->setAuthor($parameters['author']);
-        $book->setDescription($parameters['description']);
-        $book->setPublishDate(new \DateTime($parameters['publishDate']));
-        $book->setIsbn($parameters['isbn']);
-        $book->setCoverPhoto($parameters['coverPhoto']);
+            $book = $entityManager->getRepository(Book::class)->findBy(['title' => $parameters['title']]);
+            if ($book) {
+                return $this->json('book found for title ' . $parameters['title'], 400);
+            }
     
-        $entityManager->persist($book);
-        $entityManager->flush();
+            $book = new Book();
+            $book->setTitle($parameters['title']);
+            $book->setAuthor($parameters['author']);
+            $book->setPublishDate(new \DateTime($parameters['publishDate']));
+            $book->setIsbn($parameters['isbn']);
+            $book->setCoverPhoto($parameters['coverPhoto']);
     
-        $data =  $this->shapeResponse($book);
-
-            
-        return $this->json($data);
+            if (isset($parameters['description'])) { $book->setDescription($parameters['description']); }
+        
+            $entityManager->persist($book);
+            $entityManager->flush();
+        
+            $data =  $this->shapeResponse($book);
+    
+                
+            return $this->json($data);
+        } catch (\Exception $ex) {
+            if (get_class($ex) === 'App\Exception\ValidationException') {
+                return $this->json([
+                    'status' => Response::HTTP_BAD_REQUEST,
+                    'errors' => json_decode($ex->getMessage()),
+                ], Response::HTTP_BAD_REQUEST);
+            }
+            return $this->json('There is a problem with page. Please contact with support 666-666-666', Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
   
     #[Route('/books/{id}', name: 'book_show', methods:['get'] )]
@@ -116,5 +133,21 @@ class BookController extends AbstractController
             'isbn' => $book->getIsbn(),
             'coverPhoto' => $book->getCoverPhoto(),
         ];
+    }
+
+    private function simpleDataValdiation(Array $parameters): void
+    {
+        $errors = [];
+
+        if (!isset($parameters['title'])) { $errors[] = 'title is required'; }
+        if (!isset($parameters['author'])) { $errors[] = 'author is required'; }
+        if (!isset($parameters['publishDate'])) { $errors[] = 'publish date is required'; }
+        if (!isset($parameters['isbn'])) { $errors[] = 'ISBN is required'; }
+        if (!isset($parameters['coverPhoto'])) { $errors[] = 'cover photo is required'; }
+        if (!isset($parameters['title'])) { $errors[] = 'Title is required'; }
+
+        if (count($errors) > 0) {
+            throw new ValidationException(json_encode($errors));
+        }
     }
 }
