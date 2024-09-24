8bb55a1635fbfa4977e515df831798e0b466070b
entymon <pawel.olejniczak.i@gmail.com>
2024-09-24 14:58:39 +0200

    added fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,45 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Book;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
+
 class AppFixtures extends Fixture
 {
+    private $authors = [
+        'Jordan',
+        'Pawel',
+        'Agatha',
+        'Judy',
+        'Robert',
+        'Alex',
+        'Neron',
+        'Jose',
+        'Maria',
+    ];
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($n = 1; $n <= 100; $n++) {
+            for ($i = 1; $i <= 10000; $i++) {
+                $author = $this->authors[rand(0, count($this->authors) - 1)];
+                $date = new \DateTimeImmutable('2000-01-01');
 
-        $manager->flush();
+                $book = new Book();
+                $book->setTitle('Kubuś Puchatek nr'. uniqid());
+                $book->setAuthor($author);
+                $book->setDescription('A pure book of drummers turned singers! author of the: '. $author);
+                $book->setPublishDate($date);
+                $book->setIsbn('9781472911292');
+                $book->setCoverPhoto('http://localhost:8100/images/cat-1.jpg');
+                $manager->persist($book);
+            }
+
+            $manager->flush();
+            sleep(10);
+        }
+
     }
 }
